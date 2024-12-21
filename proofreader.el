d13d5ebe77f5ed940c9d617b371b869133d20736
Joe Reinhart <joseph.reinhart@gmail.com>
2024-12-21 13:23:09 -0800

    Initial check-in of some basic files for the project

---
+++ proofreader.el
@@ -0,0 +1,40 @@
+;;; proofreader --- Proof-reading tool for Eamcs -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright (c) 2024 Joe Reinhart <joseph.reinhart@gmail.com>
+
+;; Author: Joe Reinhart
+;; URL: https://github.com/jayemar/proofreader
+;; Keywords: proofreader, writing
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;; Proofreader provides tooling to help with proof-reading compositions.
+
+;;; Code:
+
+;;;###autoload
+(define-minor-mode proofreader-mode
+  "Toggle minor mode `proofreader-mode' on and off."
+  :init-value nil
+  :lighter " pf")
+
+(provide 'proofreader)
+;;; proofreader.el ends here
